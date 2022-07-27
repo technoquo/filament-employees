6b09830d242f9c19cb94766c44539e89c417d5de
Leonel <lelobo75@gmail.com>
2022-07-27 09:38:25 +0200

    relation managers fix form and table

--- app/Filament/Resources/EmployeeResource/Widgets/EmployeeStatsOverview.php
+++ app/Filament/Resources/EmployeeResource/Widgets/EmployeeStatsOverview.php
@@ -4,8 +4,8 @@ namespace App\Filament\Resources\EmployeeResource\Widgets;
 
 use App\Models\Country;
 use App\Models\Employee;
-use Filament\Widgets\StatsOverviewWidget\Card;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
 
 class EmployeeStatsOverview extends BaseWidget
 {
@@ -13,11 +13,11 @@ class EmployeeStatsOverview extends BaseWidget
     {
         $us = Country::where('country_code', 'US')->withCount('employees')->first();
         $cr = Country::where('country_code', 'CR')->withCount('employees')->first();
-        
+
         return [
             Card::make('All Employees', Employee::all()->count()),
-            Card::make($us->name. ' Employees', $us->employees_count),
-            Card::make($cr->name. ' Employees', $cr->employees_count),
+            Card::make('CR Employees', $cr ? $cr->employees_count : 0),
+            Card::make('US Employees', $us ? $us->employees_count : 0),
         ];
     }
 }
